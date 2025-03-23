474ba9001763ba2009055d59b68d72415cff3e2e
Arthur <thanagorn2005@gmail.com>
2025-03-23 20:48:07 +0700

    fix path

--- project-sleepy/src/app/page.tsx
+++ project-sleepy/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Homapage from "@/components/Homepage";
+import Homepage from "@/components/Homepage";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Homapage/>
+      <Homepage/>
     </main>
   );
 }
