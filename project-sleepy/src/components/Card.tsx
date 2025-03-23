182a5c467226a6fc4c98a500b3ecd3967cb977d4
Arthur <thanagorn2005@gmail.com>
2025-03-23 18:46:37 +0700

    add react-icons and fix position of card

--- project-sleepy/src/components/Card.tsx
+++ project-sleepy/src/components/Card.tsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 import { MSItem } from "../../interface"
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import { FaLocationDot } from "react-icons/fa6"
 
 export default function Card({ MSItem }: { MSItem: MSItem }) {
     const [toggle, setToggle] = useState(false);
@@ -13,9 +14,9 @@ export default function Card({ MSItem }: { MSItem: MSItem }) {
             onClick={() => setToggle(prev => !prev)}> 
             <div className="flex flex-col items-start px-7 py-3">
                 <h1 className="text-xl font-bold">{MSItem.name}</h1>
-                <h3 className='text-l font-bold'>5 Star</h3>
-                <h5> {MSItem.province}</h5>
-                <div className={`transition-all duration-300 ease-in-out overflow-hidden ${toggle ? "h-[200px] p-2" : "h-0 p-0"}`}>
+                <h3 className='text-xl '>5 Star</h3>
+                <h5 className='flex flex-row mt-4'><FaLocationDot/> <div className='mx-1'>{MSItem.province}</div></h5>
+                <div className={`flex flex-col items-start transition-all duration-300 ease-in-out overflow-hidden ${toggle ? "h-[240px] p-2" : "h-0 p-0"}`}>
                     <p>Address: {MSItem.address}</p>
                     <p>District: {MSItem.district}</p>
                     <p>Province: {MSItem.province}</p>
@@ -23,6 +24,11 @@ export default function Card({ MSItem }: { MSItem: MSItem }) {
                     <p>Tel: {MSItem.tel}</p>
                     <p>Open: {MSItem.openTime}</p>
                     <p>Close: {MSItem.closeTime}</p>
+                    <button className='bg-amber-800 text-white border-cyan-600 font-semibold py-2 px-2 my-2 roumded z-30  
+                        hover:bg-cyan-600 hover:text-white hover:border-transparent'
+                        onClick={(e) => { e.stopPropagation; router.push('/booking') }}>
+                    Booking
+                    </button>
                 </div>
             </div> 
         </div>
